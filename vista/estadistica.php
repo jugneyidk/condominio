bf51704e3e3f29017a4193ff0c2178382b382b6d
jugneyidk <jugneyv@gmail.com>
2023-09-11 10:28:12 -0400

    V1.15

--- vista/estadistica.php
+++ vista/estadistica.php
@@ -12,7 +12,46 @@
 		</div>
 
 
+<div class="container">
+       <h2>Morosos</h2>
+        <div class="chart-container">
+            <canvas id="myChart" width="100" height="220"></canvas>
+        </div>
+    </div>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
+    <script>
+        // Datos de ejemplo
+        var labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+        var data = [3, 1, 5, 2, 4];
 
+        var ctx = document.getElementById("myChart").getContext("2d");
+
+        new Chart(ctx, {
+            type: "bar",
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        label: "Morosos",
+                        data: data,
+                        backgroundColor: "rgba(75, 192, 192, 0.2)",
+                        borderColor: "rgba(75, 192, 192, 1)",
+                        borderWidth: 1,
+                    },
+                ],
+            },
+            options: {
+            	responsive: true, // Permite que el gráfico sea responsive
+        maintainAspectRatio: false, // Evita que el gráfico mantenga un aspect ratio
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+                },
+            },
+        });
+    </script>
 
 
 	</div>
--- vista/movimiento.php
+++ vista/movimiento.php
@@ -1,6 +1,32 @@
-<?php require_once('comunes/head.php'); ?>
-<?php require_once('comunes/head.php'); ?>
 
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+        }
+
+        h1 {
+            text-align: center;
+            color: #333;
+        }
+
+        .balance {
+            text-align: center;
+            font-size: 24px;
+            margin-top: 20px;
+            color: #007bff;
+        }
+    </style>
+    <?php require_once('comunes/head.php'); ?>
 <body class="bg-light">
 	<?php require_once("comunes/carga.php"); ?>
 	<?php require_once("comunes/modal.php"); ?>
@@ -8,11 +34,20 @@
 	<div class="container-lg bg-white p-2 p-sm-4 p-md-5 mb-5">
 		<?php require_once('comunes/cabecera_modulos.php'); ?>
 		<div>
-			<h2 class="text-center h2 text-primary">Movimientos</h2>
+			<h2 class="text-center h2 text-primary">Balance condominio</h2>
 			<hr />
 		</div>
-
-
+</div>
+ <div class="container">
+        <h1>Saldo</h1>
+        <?php
+        // Simula un saldo ficticio
+        $balance = 1000.50;
+        ?>
+        <div class="balance">
+            Tu saldo actual es: $<?php echo number_format($balance, 2); ?>
+        </div>
+    </div>
 
 
 
